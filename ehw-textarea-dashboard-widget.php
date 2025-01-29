d6505c12c6450755b78938993a0e49cf60ea18fb
Eric L. Hepperle <codewizard13@gmail.com>
2025-01-29 16:25:05 -0600

    UPDATE ehw-textarea-dashboard-widget.php:  BROKEN: textcontent not display in callback

--- ehw-textarea-dashboard-widget.php
+++ ehw-textarea-dashboard-widget.php
@@ -5,11 +5,11 @@
  * Description:     Display editable textarea dashboard widget
  * Version:         01.00.00
  * Date Created:    01/20/27
+ * Date Modified:   01/29/27
  * Author:          Eric L. Hepperle
  * Author URI:      erichepperle.com
- * Developer:       Eric Hepperle
  * 
- * Inspired by WP Learn Dashboard Widgets
+ * Inspired by WP Learn Dashboard Widgets: https://www.youtube.com/watch?v=sgC9ISgPpNQ
  */
 
 add_action('wp_dashboard_setup', 'ehw_textarea_dashboard_widget');
@@ -41,57 +41,27 @@ function ehw_textarea_dashboard_widget_callback($screen, $widget_args)
     align-items: stretch;
 }
 </style>
+
+<b>Description: </b><?php echo $widget_args['args']['description'] ?><br>
+
 <?php
 
-  $numberposts = get_option('ehw_textarea_dashboard_widget_numberposts', 5);
-
-  $args = [
-    'posts_per_page' => $numberposts,
-    'post_status' => 'publish',
-    'post_type' => ['event']
-  ];
-
-  $recent_posts = wp_get_recent_posts($args);
-
-  ?>
-  <b>Description: </b><?php echo $widget_args['args']['description'] ?><br>
-  <?php
-
-  echo '<ul>';
-
-  foreach ($recent_posts as $recent_post) {
-    $postId = $recent_post['ID'];
-
-    if ( has_post_thumbnail( $postId ) ) {
-      $postThumb = get_the_post_thumbnail($postId, [40,40]);
-    }
-
-    $postTitle = function_exists('elsm_trunc_text') ? elsm_trunc_text($recent_post['post_title'], 64) : $recent_post['post_title'];
-
-    $post_row = '<li class="ehw-dash-row"><a href="' . get_permalink($postId) . '">';
-    $post_row .= $postThumb ? $postThumb : '';
-    $post_row .= '<span class="ehw-post-title">' . $postTitle . '</span>';
-    $post_row .= '</a></li>';
-    echo $post_row;
-
-  }
-
-  echo '</ul>';
-
-
+  $textcontent = get_option('ehw_textarea_dashboard_widget_textcontent', 'Default dummy textcontent');
+  echo '<textarea id="results" name="ehw_textarea_dashboard_widget_textcontent" readonly>'. $textcontent . '</textarea>';
 
 }
 
 
-
 function ehw_textarea_dashboard_widget_control()
 {
-  if (isset($_POST['ehw_textarea_dashboard_widget_numberposts'])) {
-    $numberposts = sanitize_text_field($_POST['ehw_textarea_dashboard_widget_numberposts']);
-    update_option('ehw_textarea_dashboard_widget_numberposts', $numberposts);
+  if (isset($_POST['ehw_textarea_dashboard_widget_textcontent'])) {
+    $numberposts = sanitize_text_field($_POST['ehw_textarea_dashboard_widget_textcontent']);
+    update_option('ehw_textarea_dashboard_widget_textcontent', $textcontent);
   }
 
-  $numberposts = get_option('ehw_textarea_dashboard_widget_numberposts', 5);
-  echo '<label>Enter the number of posts to display</label>';
-  echo '<input type="text" name="ehw_textarea_dashboard_widget_numberposts" value="' . $numberposts . '" />';
+  $numberposts = get_option('ehw_textarea_dashboard_widget_textcontent', 5);
+  echo '<label>Enter some text:</label>';
+  echo '<textarea id="textcontent" name="ehw_textarea_dashboard_widget_textcontent" name="textcontent" rows="5" cols="33" >';
+  echo 'Starter Text' . '</textarea>';
+
 }
