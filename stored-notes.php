275752ea7f46d6db03a857a58c514c6f6dcc17e5
Eric L. Hepperle <codewizard13@gmail.com>
2025-02-03 15:48:34 -0600

    UPDATE stored-notes.php

--- stored-notes.php
+++ stored-notes.php
@@ -1,9 +1,2 @@
-
-<h1>Testing Site Notes!</h1>
-<ul>
-    <li>Lorem ipsum dolor sit amet, consectetur adipisicing.</li>
-    <li>Est dicta tenetur soluta enim fuga odit!</li>
-    <li>Omnis exercitationem nesciunt beatae fugiat. Laborum, natus!</li>
-    <li>Blanditiis quis minus in veritatis sunt quod!</li>
-    <li>Distinctio vel provident saepe quidem praesentium commodi.</li>
-</ul>
+<b>SITE NAME:</b> ES-PROD-807<br>
+<b>SITE PURPOSE:</b> Use 8.07 version of ElijahStreams.com to test custom plugins for debugging and development
